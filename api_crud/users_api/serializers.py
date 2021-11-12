5eb89ea65531d1a0dc68445f7a127c1e65a38a29
zik59 <mihael-keel@mail.ru>
2021-11-12 19:08:25 +0300

    one serializer, and fewer code in views

--- api_crud/users_api/serializers.py
+++ api_crud/users_api/serializers.py
@@ -1,11 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
-class ReadOnlyUserSerializer(serializers.ModelSerializer):
+     
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'first_name', 'last_name', 'is_active', 'last_login', 'is_superuser']        
-class WriteOnlyUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'first_name', 'last_name', 'password', 'is_active']
+        fields = ['id', 'username', 'first_name', 'last_name', 'password', 'is_active', 'last_login', 'is_superuser']
--- api_crud/users_api/views.py
+++ api_crud/users_api/views.py
@@ -1,37 +1,10 @@
-from rest_framework import serializers, viewsets
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import viewsets
 from django.contrib.auth.models import User
-from users_api.serializers import ReadOnlyUserSerializer, WriteOnlyUserSerializer
+from users_api.serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     """"API endpoint that allows users to be viewed or edited"""
     queryset = User.objects.all()
-    serializer_class = ReadOnlyUserSerializer
-    
-    def create(self, request):
-        serializer = WriteOnlyUserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            user_read_serializer = ReadOnlyUserSerializer()
-            return Response(user_read_serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def update(self, request, pk=None):  
-        serializer = WriteOnlyUserSerializer(data=request.data)              
-        if serializer.is_valid():
-            serializer.save()
-            user = User.objects.get(pk=pk)
-            user_read_serializer = ReadOnlyUserSerializer(user)
-            return Response(user_read_serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def partial_update(self, request, pk=None):    
-        serializer = WriteOnlyUserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            user = User.objects.get(pk=pk)
-            user_read_serializer = ReadOnlyUserSerializer(user)
-            return Response(user_read_serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer_class = UserSerializer
+
     
